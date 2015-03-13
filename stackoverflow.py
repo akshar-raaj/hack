bee80de026515400db359e6b332d064c35d8945e
Akshar Raaj <akshar@agiliq.com>
2015-03-13 12:07:44 +0530

    Fetch answers on a particular list page parallely, using threads. Much faster

--- stackoverflow.py
+++ stackoverflow.py
@@ -1,13 +1,19 @@
-#40350
+#40368
 import requests
+from requests.exceptions import ConnectionError
 from threading import Thread
+import time
+from threading import Lock
 from BeautifulSoup import BeautifulSoup
 
 base_url = "http://stackoverflow.com/users/839549/akshar?tab=answers&sort=activity&page="
+#base_url = "http://stackoverflow.com/users/104349/daniel-roseman?tab=answers&sort=activity&page="
 stackoverflow_url = "http://stackoverflow.com"
 base_page_number = 1
 first_page = base_url + "1"
 num_views = 0
+lock = Lock()
+should_wait = False
 
 class GetUrlThread(Thread):
     def __init__(self, url):
@@ -15,11 +21,23 @@ class GetUrlThread(Thread):
         super(GetUrlThread, self).__init__()
 
     def run(self):
-        resp = get_single_question_response(self.url)
-        return resp
+        global num_views
+        global should_wait
+        try:
+            if should_wait:
+                time.sleep(10)
+            should_wait = False
+            resp = requests.get(self.url)
+        except ConnectionError:
+            print "Connection Error happened"
+            should_wait = True
+        view_count = get_view_count(resp)
+        lock.acquire()
+        num_views = num_views + view_count
+        lock.release()
 
 
-def get_view_count(question_soup):
+def get_view_count(question_href_resp):
     """
     Give a BS instance containing response of a SO question page
     Find the view count on this page
@@ -44,9 +62,15 @@ def get_question_absolute_href(accepted_answer):
     absolute_question_href = stackoverflow_url + question_href
     return absolute_question_href
 
-def get_single_question_response(url):
-    print url
-    return requests.get(url)
+#single_answer_list_threads = []
+#class SingleAnswerListThread(Thread):
+    #def __init__(self, url, number):
+        #self.url = url
+
+    #def run(self):
+        #resp = requests.get(url)
+        #soup = BeautifulSoup(resp.content)
+        #accepted_answer_divs = soup.findAll("div", {"class": "answer-votes answered-accepted default"})
 
 while True:
     url = base_url + str(base_page_number)
@@ -56,10 +80,11 @@ while True:
     threads = []
     for accepted_answer in accepted_answer_divs:
         absolute_question_href = get_question_absolute_href(accepted_answer)
-        question_href_resp = get_single_question_response(absolute_question_href)
-        view_count = get_view_count(question_href_resp)
-        num_views = num_views + view_count
-    break
+        t = GetUrlThread(absolute_question_href)
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
     next_page = soup.find("span", {"class": "page-numbers next"})
     if not next_page:
         break
